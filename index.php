9048cf7f3ba5c77e5477cdfdc458a45a1fd090ad
Gianluigi <a35g@hackworld.it>
2023-11-20 18:23:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 
-require_once 'inc/Core.php';
-use gmCaptcha\Core;
+require_once './vendor/autoload.php';
+use App\Core\Core;
+$captcha = new Core();
 
-$gmc = new Core;
-
-if (NULL !== $output):
-	$output = $gmc->makeGraphic("T");
-	echo sprintf("<img src='data:image/png;base64,%s' />", $output);
-else:
-	echo "GD not even installed.";
-	exit;
-endif;
+$captcha->makeGraphic("T");
