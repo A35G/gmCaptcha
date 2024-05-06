429bad76a07b70b1c91bc45175feafcd3e47eb1e
Gianluigi <a35g@hackworld.it>
2024-05-06 18:46:59 +0200

    Update Error.php

--- app/core/Error.php
+++ app/core/Error.php
@@ -4,38 +4,37 @@ namespace App\Error;
 
 class Error
 {
-    private $makeErrorFile = false;
+    private static $makeErrorFile = false;
 
-    private $logDir;
+    private static $logDir;
 
     public function __construct() {
         $appDir = realpath(__DIR__ . "/../");
-        $this->setLogDir($appDir . "/logs");
-
-        $this->checkPermission();
+        self::setLogDir($appDir . "/logs");
     }
 
-    private function setLogDir(string $path): void {
+    private static function setLogDir(string $path): void {
         if (file_exists($path) && is_dir($path)) {
-            $this->logDir = $path;
+            self::$logDir = $path;
+            self::checkPermission();
         }
     }
 
-    private function checkPermission(): void
+    private static function checkPermission(): void
     {
-        if (is_writable($this->logDir)) {
-            $this->makeErrorFile = true;
+        if (is_writable(self::$logDir)) {
+            self::$makeErrorFile = true;
         }
     }
 
-    public function logError(string $textError) {
-        if ($this->makeErrorFile) {
+    public static function logError(string $textError) {
+        if (self::$makeErrorFile) {
             $logFile = "log-" . date("Y-m-d") . ".php";
 
             $dataError = "[client " . $_SERVER['REMOTE_ADDR'] . "] ERROR: " .
              $textError;
 
-            file_put_contents($this->logDir . "/" . $logFile, $dataError, 
+            file_put_contents(self::$logDir . "/" . $logFile, $dataError, 
                 FILE_APPEND | LOCK_EX);
         }
     }
